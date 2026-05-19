9e2f8f89ec334e43d93596cb7069d08ff304fceb
Kelwing <kelwing@kelnet.org>
2026-05-19 09:24:17 -0400

    feat(dns): add headscale record

--- dns.tf
+++ dns.tf
@@ -5,6 +5,14 @@ data "cloudflare_zone" "main" {
   }
 }
 
+resource "cloudflare_dns_record" "headscale" {
+  zone_id = data.cloudflare_zone.main.zone_id
+  name = "headscale"
+  content = "136.118.241.125"
+  type = "A"
+  proxied = false
+}
+
 # Add DNS records below. Example:
 # resource "cloudflare_dns_record" "example" {
 #   zone_id = data.cloudflare_zone.main.zone_id
